c432f0c9d7e4c5529e3f4ca9f970df093ab8443c
Joy <joydas21891920@gmail.com>
2024-02-04 13:31:29 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,8 @@
+let firstName = "Joy";
+let lastName = "Das";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+console.log(fullName.toLocaleLowerCase());
+console.log(fullName.toLocaleUpperCase());
